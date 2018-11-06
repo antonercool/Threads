91f86307e258a19740a3c73fcd5814d5235b9b6b
anton <antonsihm@gmail.com>
2018-11-06 20:30:42 +0100

    HelloWriter Created Tests with BackGround and Foreground threads

---
+++ TheadExercise/TheadExercise/HelloWriter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ThreadsExercise
+{
+    public class HelloWriter
+    {
+        public string Name { get; set; }
+        public int NumberOfTimesToLoop{ get; set; }
+
+
+        public HelloWriter(string name, int numberOfTimesToLoop)
+        {
+            NumberOfTimesToLoop = numberOfTimesToLoop;
+            Name = name;
+        }
+        public void SayHello()
+         {
+             for (int i = 0; i < NumberOfTimesToLoop; i++)
+             {
+                 Console.WriteLine($"Hello from {Name} : {i}");
+             }
+        }
+
+        public void SayHello2(object numberOfTimes)
+        {
+            for (int i = 0; i < (int)numberOfTimes; i++)
+            {
+                Console.WriteLine($"Hello from {Name} : {i}");
+            }
+        }
+
+    }
+    
+}
--- TheadExercise/TheadExercise/Program.cs
+++ TheadExercise/TheadExercise/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Threading;
+using ThreadsExercise;
 
 namespace TheadExercise
 {
@@ -6,7 +8,24 @@ namespace TheadExercise
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            HelloWriter helloWriter1 = new HelloWriter("HelloWriter1", 1000);
+            HelloWriter helloWriter2 = new HelloWriter("HelloWriter2", 2000);
+
+
+            Thread thread1 = new Thread(helloWriter1.SayHello2);
+            thread1.Start(1000);
+            //thread1.IsBackground = true;
+
+            Thread thread2 = new Thread(helloWriter2.SayHello2);
+            thread2.Start(2000);
+            //thread2.IsBackground = true;
+
+
+
+
+
+            Console.ReadKey();
+            Console.WriteLine("Main is done");
         }
     }
 }
